357f668e9a79bdeb1fa6e00273e84cff0f278f47
raviteja182 <gravteja9@gmail.com>
2025-04-29 21:48:55 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 <?php
 
-    $con = mysqli_connect("w2tg1ftzn3kcdv3j@lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","l5q178u6s05229kt","w2tg1ftzn3kcdv3j","tqo2l6zen7mwgju4");
+    $con = mysqli_connect("lg7j30weuqckmw07.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","l5q178u6s05229kt","w2tg1ftzn3kcdv3j","tqo2l6zen7mwgju4");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
