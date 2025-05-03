5dd6f7c7b32967cb2e5c8a729baf5f66399cfa6f
raviteja182 <gravteja9@gmail.com>
2025-05-03 08:53:35 +0530

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -6,6 +6,6 @@
 	mysqli_query($con,"update `users` set email='$email', phnum='$phnum' where username='$username'");
 	echo "<div class='form'>
     <h3>Updated succesfully</h3><br/>
-    <p class='link'>Click here to see vehicles<a href='Home.php'>Home</a></p>
+    <p class='link'>Click here to see vehicles<a href='home.php'>Home</a></p>
     </div>";
 ?>
