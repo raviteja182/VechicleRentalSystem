89cd29641483309fb6179e5623d60b68c0980ff7
raviteja182 <gravteja9@gmail.com>
2025-05-03 08:50:05 +0530

    Update update.php

--- update.php
+++ update.php
@@ -6,6 +6,6 @@
 	mysqli_query($con,"update `vehicles` set price='$price', description='$description' where vehicle_id='$vehicle_id'");
 	echo "<div class='form'>
     <h3>Updated succesfully</h3><br/>
-    <p class='link'>Click here to see items<a href='Home.php'>Home</a></p>
+    <p class='link'>Click here to see items<a href='home.php'>Home</a></p>
     </div>";
 ?>
